6e3119c2504684bed36be2fb806c64f83e9e3aa1
minhthu2610 <minhthu2xx5@gmail.com>
2026-07-08 16:17:28 +0700

    Fix typo in student name in index.php

--- LTMNMBuoi02/baitap08_QuanLySinhVien/index.php
+++ LTMNMBuoi02/baitap08_QuanLySinhVien/index.php
@@ -1,5 +1,5 @@
 <?php
 require_once "autoload.php";
 use App\Students\Student;
-$sv = new Student("Nguyễn Thị Minh Thu",21,"SV001");
+$sv = new Student("Nguyễn Thị Minh Thư",21,"SV001");
 $sv->hienThi();
